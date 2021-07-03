6d8bbc58b4b635055db35136e3449bc232e94892
Ben <ben@steadfast.co.nz>
2021-07-03 17:59:24 +1200

    Add ToResourceId helper for char*

--- src/Avokii/Resources/ResourceId.hpp
+++ src/Avokii/Resources/ResourceId.hpp
@@ -8,5 +8,6 @@ namespace Avokii
 	using ResourceId_T = HashedString::hash_type;
 
 	inline ResourceId_T ToResourceId( const String value ) noexcept { return ResourceId{ value.data() }.value(); }
+	inline constexpr ResourceId_T ToResourceId( const Char* const value ) noexcept { return ResourceId{ value }.value(); }
 	inline constexpr ResourceId_T ToResourceId( StringView value ) noexcept { return ResourceId{ value.data() }.value(); }
 }
